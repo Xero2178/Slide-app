e46032d10abadf2ce06a729ced49c7ee8bf1e312
Dennis.js <complexdennis@gmail.com>
2023-10-06 15:31:57 +0100

    Added a show detail functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 
+import { logDOM } from "@testing-library/react";
+import { click } from "@testing-library/user-event/dist/click";
 import { useState } from "react"
 
 const reactFacts = [
@@ -6,6 +8,7 @@ const reactFacts = [
         id: 1,
         fact: "React is a Library, not a Framework",
         detail: "React has always been most times seen as a framework, but infact, is actually a library. A framework is an external file which allows access for external files to make use of its contents, a library on the other hand, is a storage, which mutates or makes writing vanilla and crude codes easier, by implementing declarative and simple codes.",
+        showDetail: true,
         likes: 12,
         likeStatus: false
     },
@@ -13,6 +16,7 @@ const reactFacts = [
         id: 2,
         fact: "React's ecosystem is the largest currently",
         detail: "Ever since react was intoduced, it has greatly improved the speed at which jobs get done, by avoiding the manipulation of the virtual DOM, react gives the developer full access to its abilities",
+        showDetail: true,
         likes: 4,
         likeStatus: false
     },
@@ -20,6 +24,7 @@ const reactFacts = [
         id: 3,
         fact: "We use JSX in React not HTML",
         detail: "A common misconception is that, React allows writing HTML, but NO, React allows writing of JSX, which stands for Javascript XML, although they have similar structure, they are actually quite different, in the fact that one can write Javascript logic directly in his or her JSX",
+        showDetail: true,
         likes: 9,
         likeStatus: false
     },
@@ -27,6 +32,7 @@ const reactFacts = [
         id: 4,
         fact: "Just Use React",
         detail: "If you want fast, reusable, flexible and declarative code, where you don't always have to tell Javascript how to do something, to get it done, in other words, you don't actually fancy imperative code, just use React.",
+        showDetail: true,
         likes: 21,
         likeStatus: false
     }
@@ -47,6 +53,10 @@ export default function App() {
     function handleSetArray(updatedValue, likeStat) {
         setArray(array.map((obj) => selected+1 === obj.id ? {...obj, likes: updatedValue, likeStatus: likeStat} : obj))
       }
+
+      function handleSetDetail(detail) {
+        setArray(array.map((obj) => selected+1 === obj.id ? {...obj, showDetail: detail} : obj))
+      }
     
 
       function handleSetSelected(num) {
@@ -57,10 +67,10 @@ export default function App() {
     return(
         <div className="mx-0 px-4 grid">
             <div className="flex gap-4">
-                {reactFact.map((fact) => <Tab id={fact.id} onSetSelected={handleSetSelected} selected={selected}/>)}
+                {reactFact.map((fact, i) => <Tab id={fact.id} onSetSelected={handleSetSelected} selected={selected} key={i}/>)}
             </div>
             <div>
-                <TabContent selected={selected} array={array} onSetArray={handleSetArray}/>
+                <TabContent selected={selected} array={array} onSetArray={handleSetArray} onSetDetail={handleSetDetail}/>
             </div>
 
         </div>
@@ -75,6 +85,11 @@ function Tab({id, onSetSelected, selected}) {
     const isClicked = reactFact.map((fact) => fact.id === selected + 1)[id-1]
 
 
+    const clicked = reactFact.includes((fact) => fact.id === selected + 1)
+    
+    // console.log(id);
+
+    // console.log(clicked);
     
 
     
@@ -87,16 +102,22 @@ function Tab({id, onSetSelected, selected}) {
     )
 }
 
-function TabContent({selected, array, onSetArray}) {
+function TabContent({selected, array, onSetArray, onSetDetail}) {
+
+   const curTab = array[selected]
 
     
+    
+    
+    console.log(array[selected].showDetail);
    
     return(
         <div className="w-3/4 max-w-xl  border-2 border-blue-600  p-4 rounded-xl bg-sky-200">
             <div className="mb-5">
-            <h1 className="font-bold text-2xl mb-5 text-sky-700">{array[selected].fact}</h1>
-            <p>{array[selected].detail}</p>
+            <h1 className="font-bold text-2xl mb-5 text-sky-700">{curTab.fact}</h1>
+            {curTab.showDetail && <p>{curTab.detail}</p>}
             </div>
+            <p className="underline text-purple-700 mb-4 cursor-pointer" onClick={() => onSetDetail(!curTab.showDetail)}>{curTab.showDetail ? 'Hide details' : 'Show details'}</p>
             <div className="grid gap-4 grid-cols-3">
                 <Button>Check out article</Button>
                 <Button>Follow page</Button>
